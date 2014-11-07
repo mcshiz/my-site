8a580f2fe84109383ed4f47a0923f05384384671
Brian <mccabj0210@gmail.com>
2014-11-06 16:32:21 -0800

    initial upload

---
+++ app/scripts/main.js
@@ -0,0 +1,52 @@
+'use strict';
+$(document).ready(function(){
+  $('ul.nav').find('a').on('click', function(e) {
+    e.preventDefault();
+    var targetLi = $(this),
+        target = targetLi.attr('href'),
+        offsetValue = $(this).data('offset') || 0;
+    $('html, body').animate({
+      scrollTop: $(target).offset().top-offsetValue
+     }, 1000);
+    targetLi.parent('li').addClass('active');
+    targetLi.parent('li').siblings('li').removeClass('active');
+  });
+});
+
+
+$(document).ready(function(){
+  $('#InputName').on('blur', function(){
+    if ($(this).val().length >= 3){
+      $(this).next('span').css('color', 'green');
+    }
+    else {
+      $(this).next('span').css('color', 'red');
+    }
+  });
+  $('#InputMessage').on('blur', function(){
+    if ($(this).val().length >= 3){
+      $(this).next('span').css('color', 'green');
+    }
+    else {
+      $(this).next('span').css('color', 'red');
+    }
+  });  
+  $('#InputEmail').on('blur', function(){
+    var validEmail = $(this).val().indexOf('@') !==-1;
+    if (validEmail){
+      $(this).next('span').css('color', 'green');
+    }
+    else {
+      $(this).next('span').css('color', 'red');
+    }
+  });
+  $('#InputReal').on('blur', function(){
+    var spam = $(this).val() === 7;
+    if (spam){
+      $(this).next('span').css('color', 'green');
+    }
+    else {
+      $(this).next('span').css('color', 'red');
+    }
+  });    
+});
